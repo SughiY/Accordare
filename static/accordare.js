807ff84d5489458dfd3360b07e7f02d06be23ab7
SughiY <cedricku@icloud.com>
2016-02-03 18:17:55 +0100

    reorganize the js scripts and add mirco record from browser! but the record is too short....

--- static/accordare.js
+++ static/accordare.js
@@ -2,11 +2,38 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = null;
 
+function error() {
+	    alert('Stream generation failed.');
+}
+
+function intercationWithServer(){
+	var socket = WSEvent.connect('ws:127.0.0.1:3012') 
+		var sender = document.getElementById('message_box') 
+		sender.onchange = function(target){ 
+			var text = target.target.value 
+				socket.send(text)                                 
+		} 
+	socket.addListener('foo', function(data){ 
+		console.log(data) 
+	})
+}
+
 window.onload = function() {
 	audioContext = new AudioContext()
 
-	navigator.getUserMedia({audio: true}, function(stream) {
-		  var microphone = context.createMediaStreamSource(stream)
-		  microphone.connect(context.destination)
-	}, errorCallback)
+	navigator.getUserMedia = 
+		navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia
+
+		navigator.getUserMedia({"audio":true}, function(stream) {
+		  var microphone = audioContext.createMediaStreamSource(stream)
+		  microphone.connect(audioContext.destination)
+	}, error)
+
+	intercationWithServer()
 }
+
+
+
+
