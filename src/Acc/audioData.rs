3bdeb30948394772eb9081a40c3dd1a2d1f12cf8
SughiY <cedricku@icloud.com>
2016-02-08 01:43:45 +0100

    add two api :  sample_from_file  sample_from_buf

--- src/Acc/audioData.rs
+++ src/Acc/audioData.rs
@@ -1,8 +1,25 @@
 
 extern crate hound;
 
-pub fn sample() -> Vec<i16> {
-    match hound::WavReader::open("/Users/user/Dropbox/cours/SI/WEBAPP/accordare/src/Acc/openE.wav") {
+use std::io::{Read, BufReader};
+use std::path::Path;
+
+pub static FILENAME:&'static str = "/Users/user/Dropbox/cours/SI/WEBAPP/accordare/src/Acc/openE.wav";
+
+pub fn sample_from_file<P: AsRef<Path>>(filename:P) -> Vec<i16> {
+    let result = hound::WavReader::open(filename);
+    sample(result)
+}
+
+pub fn sample_from_buf(buf:Vec<u8>) -> Vec<i16> {
+    let slice_buf = &buf[..];
+    let result = hound::WavReader::new( slice_buf );
+    sample(result)
+}
+
+    pub fn sample<R:Read>(res : hound::Result<hound::WavReader<R>>) -> Vec<i16>
+{
+    match res {
         Err(err)=> {
             println!("openWave error{:?}",err); 
             vec![]
